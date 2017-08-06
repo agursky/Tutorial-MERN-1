6e1d78eeb4d8d8e2c225e554b002911d6a1925e9
Andrew Gursky <agursky.js@gmail.com>
2017-08-06 12:11:15 -0400

    Made add school reset state after adding school. Added modules necessary for backend.

--- app/components/AddSchool.jsx
+++ app/components/AddSchool.jsx
@@ -11,13 +11,14 @@ module.exports = React.createClass({
     addSchool:function(e){
         e.preventDefault();
         actions.addSchool(this.state);
+        this.setState({name:'', tagline:''});
     },
     handleInputChange:function(e){
       e.preventDefault();
       var name = e.target.name;
       var state = this.state;
       state[name] = e.target.value;
-      this.setState(state);
+      this.setState(state);   
     },
     render:function(){
         return(
@@ -28,7 +29,7 @@ module.exports = React.createClass({
                 </div>
                 <div className="form-group">
                     <label className="control-label" htmlFor="tagline">Tagline:</label>
-                    <input type="text" className="form-control" id="tagline" name="tagline" value={this.state.address} onChange={this.handleInputChange} placeholder="Tagline" />                    
+                    <input type="text" className="form-control" id="tagline" name="tagline" value={this.state.tagline} onChange={this.handleInputChange} placeholder="Tagline" />                    
                 </div>
                 <div className="form-group">
                     <button className="btn" type="submit">Add School</button>
