21cda07788f0a873d4c071af5b620e83a7a25062
Andrew Gursky <agursky.js@gmail.com>
2017-08-06 11:42:54 -0400

    Added dispatcher and store. Added actions functionality.

--- app/main.jsx
+++ app/main.jsx
@@ -1,9 +1,12 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 var SchoolsList = require("./components/SchoolsList.jsx");
-
-var _schools = [{name:"Lovedale",tagline:"this is a wonderful school"},
-                {name:"Bishop",tagline:"Another great school"}];
+var schoolsStore = require('./stores/schoolsStore');
+var _schools = schoolsStore.getSchools();
+schoolsStore.onChange(function(schools) {
+    _schools = schools;
+    render();
+});
                 
 function render(){
     ReactDOM.render(<SchoolsList schools={_schools} />, document.getElementById("container"));    
